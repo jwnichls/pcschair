5556060780733c21245ad0e74595650c4bd40a9c
Jeffrey Nichols <jeff@jeffreynichols.com>
2017-12-02 23:02:53 -0800

    More updates for running on heroku.

--- pcs-chair-site/config/application.rb
+++ pcs-chair-site/config/application.rb
@@ -58,5 +58,8 @@ module PCSChairingAssistant
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Change asset precompiles to work better on heroku
+    config.assets.initialize_on_precompile = false
   end
 end
